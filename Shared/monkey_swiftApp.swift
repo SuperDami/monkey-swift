e7e077f2fa69c0e1e07e294bafbebf4a54f8d8af
zhejunchen-jcv <zhejun.chen@japancv.co.jp>
2022-07-22 23:48:39 +0800

    Initial Commit

---
+++ Shared/monkey_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  monkey_swiftApp.swift
+//  Shared
+//
+//  Created by Zhejun Chen on 2022/07/22.
+//
+
+import SwiftUI
+
+@main
+struct monkey_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
